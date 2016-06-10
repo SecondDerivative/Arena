f45f71bc40f78fb092be86ae38ab795e962d87fb
Никита <ulamog345@gmail.com>
2016-06-10 12:28:52 +0300

    Added player and inventory w/ items

---
+++ SFMLApp/Inventory.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFMLApp
+{
+	class Item
+	{
+		protected int Damage;
+		protected int Range;
+		public int attack(){return Damage;}
+		public int getRange() { return Range; }
+	}
+    class ItemSword : Item
+    {
+        public ItemSword(int dmg, int ran)
+        {
+             Damage = dmg;
+             Range = ran;
+        }
+     }
+        class ItemBow : Item
+        {
+            private int nArrows;
+            public ItemBow(int dmg, int ran, int arr)
+            {
+                Damage = dmg;
+                Range = ran;
+                nArrows = arr;
+            }
+            public int attack()
+            {
+                if (nArrows > 0)
+                {
+                    nArrows--;
+                    return Damage;
+                }
+                else { return 0; }
+            }
+        }
+        /*class Magic : Item
+        {
+            private int ManaCost;
+            public Magic(int dmg, int ran, int mana)
+            {
+                Damage = dmg;
+                Range = ran;
+                ManaCost = mana;
+            }
+            public int attack(ref int mana)
+            {
+                if (mana >= ManaCost)
+                {
+                    mana = mana-ManaCost;
+                    return Damage;
+                }else
+                {
+                    return 0;
+                }
+            }
+        }*/
+		class Fist : Item{
+			public Fist(){
+				Damage = 5;
+				Range = 1;
+			}
+		}
+	class Inventory
+	{
+		List<Item> inventory;
+        public Inventory()
+        {
+			inventory = new List<Item>(20);
+        }
+        public void addItem(Item item)
+        {
+            if (inventory.Count < 20)
+            {
+				inventory.Add(item);
+            }
+        }
+        public bool isInStock(Item item)
+        {
+            return inventory.Contains(item);
+        }
+        public int howMuchItems(Item item)
+        {
+            int total=0;
+            for(int i = 0; i < inventory.Count; i++)
+            {
+				if (inventory[i].Equals(item)) { total++; }
+            }
+            return total;
+        }
+    }
+}
---
+++ SFMLApp/Player.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFMLApp
+{
+    class Player
+    {
+        private int Health;
+        //private int Mana;
+        private Inventory inventory;
+		private Item leftHand;
+        private Item rightHand;
+        public Player()
+        {
+            inventory = new Inventory();
+            Health = 100;
+            //Mana = 100;
+			leftHand = new Fist ();
+			rightHand = new Fist ();
+        }
+		public int attack(){
+			int total = rightHand.attack();
+			total = total + leftHand.attack();
+			return total;
+		}
+		public void recieveDamage(int dmg){
+			if (Health > 0 & Health < dmg)
+				Health = 0;
+			else
+				Health = Health - dmg;
+    	}
+		public bool isDead(){
+			return Health > 0;
+		}
+		public void takeItemLeft(Item item){
+			leftHand = item;
+		}
+		public void takeItemRight(Item item){
+			rightHand = item;
+		}
+	}
+}
