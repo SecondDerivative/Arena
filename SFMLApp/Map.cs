3f91e3a402f8889ad301a8a814b5203bff72a084
Дмитрий Клёпов < >
2016-06-24 07:00:54 +0300

    set private && added UpdateArrow

--- SFMLApp/Map.cs
+++ SFMLApp/Map.cs
@@ -9,10 +9,12 @@ namespace SFMLApp
 {
     public class Entity
     {
-        public int x, y,r;
-        public bool Exist;
-        public string Tag;
-        public Entity(string Tag,int x,int y,int r)
+        public int r { get; private set; }
+        public double x { get; set; }
+        public double y { get; set; }
+        public bool Exist { get; set; }
+        private string Tag;
+        public Entity(string Tag,double x,double y,int r)
         {
             this.r = r;
             this.x = x;
@@ -23,25 +25,25 @@ namespace SFMLApp
     }
     public class MovableEntity : Entity
     {
-        public Tuple<int, int> Speed;
-        public MovableEntity(string Tag,int x,int y,int r):base(Tag,x,y,r){
-            this.Speed = new Tuple<int, int>(0, 0);
+        public Tuple<double, double> Speed;
+        public MovableEntity(string Tag,double x,double y,int r):base(Tag,x,y,r){
+            this.Speed = new Tuple<double, double>(0, 0);
         }
     }
     public class MPlayer : MovableEntity
     {
-        public MPlayer(string Tag,int x,int y):base(Tag, x, y, Map.RPlayer)
+        public MPlayer(string Tag,double x,double y):base(Tag, x, y, Map.RPlayer)
         {}
     }
     public class MArrow : MovableEntity
     {
-        public MArrow(string Tag,int x,int y):base(Tag, x, y, Map.RArrow)
+        public MArrow(string Tag,double x, double y):base(Tag, x, y, Map.RArrow)
         {}
     }
     public class Square
     {
-        public int x, y;
-        public bool isEmpty;
+        private int x, y;
+        public bool isEmpty { get; private set; }
         public Square(int x,int y)
         {
             this.x = x;
@@ -61,16 +63,15 @@ namespace SFMLApp
         public static int Rwidth = 32;
         public static int RArrow = 5;
 
-        
 
-        public Stopwatch Timer;
-        public int width, height;
-        public int Pwidth, Pheight;
-        Dictionary<string, MPlayer> players;
-        Dictionary<string, MArrow> arrows;
-        List<List<Square>> Field;
+        private Stopwatch Timer;
+        private int width, height;
+        private int Pwidth, Pheight;
+        private Dictionary<string, MPlayer> players;
+        private Dictionary<string, MArrow> arrows;
+        private List<List<Square>> Field;
 
-        public Map(int width, int height)
+        private Map(int width, int height)
         {
             this.width = width;
             this.height = height;
@@ -104,33 +105,91 @@ namespace SFMLApp
         {
             players[Tag].Speed = new Tuple<int, int>(0, 0);
         }
-        public void MovePlayer(string Tag,Tuple<int,int> Speed)
+        public void MovePlayer(string Tag,Tuple<double,double> Speed)
         {
             players[Tag].Speed = Speed;
         }
-        private bool IsEmpty(int x,int y)
+        private bool IsSquareEmpty(double x,double y)
         {
-            return Field[x / Rwidth][y / Rwidth].isEmpty;
+            return Field[((int)Math.Floor(x)) / Rwidth][((int)Math.Floor(y)) / Rwidth].isEmpty;
         }
         private bool IsCrossEntity(Entity a,Entity b)
         {
-            return (a.r+b.r)*(a.r+b.r)>=(a.x-b.x)*(a.x - b.x)+(a.y-b.y)* (a.y - b.y);
+            return (a.r+b.r)*(a.r+b.r)-(a.x-b.x)*(a.x - b.x)+(a.y-b.y)* (a.y - b.y)>=0.001;
         }
         private void ShortUpDatePlayer(string Tag, int Time)
         {
             MPlayer Pl = players[Tag];
-            Tuple<int, int> Line = new Tuple<int, int>(Time * Pl.Speed.Item1, Time * Pl.Speed.Item2);
-            if (!IsEmpty(Pl.x + Line.Item1, Pl.y + Line.Item1))
+            Tuple<double, double> Line = new Tuple<double, double>(Time * Pl.Speed.Item1 / 4, Time * Pl.Speed.Item2 / 4);
+            if (!IsSquareEmpty(Pl.x + Line.Item1, Pl.y + Line.Item1))
+            {
+                StopPlayer(Tag);
+                //add event;
                 return;
+            }
             players[Tag].x += Line.Item1;
             players[Tag].y += Line.Item2;
         }
-        public void UpDatePlayer(string Tag,int Time)
+        // Time/4
+        private void UpDatePlayer(string Tag,int Time)
         {
+            if (players[Tag].Speed.Item1 == 0 && players[Tag].Speed.Item2 == 0)
+                return;
             for(int i = 0; i < Time; ++i)
             {
                 ShortUpDatePlayer(Tag, 1);
+                ShortUpDatePlayer(Tag, 1);
+                ShortUpDatePlayer(Tag, 1);
+                ShortUpDatePlayer(Tag, 1);
             }
         }
+        public void FirePlayer(string Tag, Tuple<double,double> Speed)
+        {
+            arrows[Tag].Speed = Speed;
+        }
+        private void ShortUpDateArrow(string Tag, int Time)
+        {
+            MArrow Ar = arrows[Tag];
+            Tuple<double, double> Line = new Tuple<double, double>(Time * Ar.Speed.Item1 / 4, Time * Ar.Speed.Item2 / 4);
+            if (!IsSquareEmpty(Ar.x + Line.Item1, Ar.y + Line.Item1))
+            {
+                //add event
+                return;
+            }
+            arrows[Tag].x += Line.Item1;
+            arrows[Tag].y += Line.Item2;
+        }
+        private void UpDateArrow(string Tag, int Time)
+        {
+            for (int i = 0; i < Time; ++i)
+            {
+                ShortUpDateArrow(Tag, 1);
+                ShortUpDateArrow(Tag, 1);
+                ShortUpDateArrow(Tag, 1);
+                ShortUpDateArrow(Tag, 1);
+            }
+        }
+        public void UpDate()
+        {
+
+        }
+    }
+    public class MEvent
+    {
+        public MEvents Tag { get; private set; }
+        public string Tag1 { get; private set; }
+        public string Tag2 { get; private set; }
+        public MEvent(MEvents Tag,string Tag1,string Tag2)
+        {
+            this.Tag = Tag;
+            this.Tag1 = Tag1;
+            this.Tag2 = Tag2;
+        }
+    }
+    public enum MEvents
+    {
+        PlayerPlayer,
+        PlayerStone,
+        PlayerArrow
     }
 }
