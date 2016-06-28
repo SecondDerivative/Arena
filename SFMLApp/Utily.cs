71a94ff3d2629fa4e90c47f76e495dc819b31f59
prifio <tolya_ash@mail.ru>
2016-06-28 20:03:01 +0300

    Delete SFML Net dir. Add weapone. Add Utily

---
+++ SFMLApp/Utily.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFMLApp
+{
+    public static class Utily
+    {
+        static Random random = new Random();
+        public static int Next()
+        {
+            return random.Next();
+        }
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            T c = a;
+            a = b;
+            b = c;
+        }
+        public static void ChangeSeed(long seed)
+        {
+            random = new Random((int)seed);
+        }
+
+        public static double Hypot(double x, double y)
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+
+        public static int Hypot2(int x, int y)
+        {
+            return x * x + y * y;
+        }
+
+        public static double Hypot2(double x, double y)
+        {
+            return x * x + y * y;
+        }
+    }
+}
